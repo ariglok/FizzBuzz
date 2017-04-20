b3615505e99ace1a7b8c78252dbbb9166e8e9044
Gloria Carreras <gloria@chicfy.com>
2017-04-20 18:51:00 +0200

    Incluimos primer Test

---
+++ app/src/main/java/com/example/gloria/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+package com.example.gloria.fizzbuzz;
+
+/**
+ * Created by gloria on 20/4/17.
+ */
+
+public class FizzBuzz {
+    public String fizzBuzz(int value){
+        if (value == 3){
+            return "Fizz";
+        }else if (value == 5){
+            return  "Buzz";
+        }else {
+            return ""+value;
+        }
+    }
+}
---
+++ app/src/test/java/com/example/gloria/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,33 @@
+package com.example.gloria.fizzbuzz;
+
+/**
+ * Created by gloria on 20/4/17.
+ */
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void returFizzIfInputIsTree(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("Fizz",fizzBuzz.fizzBuzz(3));
+    }
+
+    @Test
+    public void returBuzzIfInputIsFive(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("Buzz",fizzBuzz.fizzBuzz(5));
+    }
+
+    @Test
+    public void returValueIfInputIsSeven(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("7",fizzBuzz.fizzBuzz(7));
+    }
+}
